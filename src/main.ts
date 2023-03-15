ecc2d017d32a681e89ca65926a444683cf917486
jed <jolix123@gmail.com>
2023-03-15 11:32:24 +0000

    commit v2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger();
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.enableCors();
+  const port = process.env.STAGE == 'prod' ? process.env.PORT : 3000;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
+
 bootstrap();
